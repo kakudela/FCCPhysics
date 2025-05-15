3d26ece1c4958048447647721d6c86f6974e1cf6
Jan Eysermans <janeysermans@gmail.com>
2025-05-15 04:54:08 -0400

    Scripts for Delphes detector studies

---
+++ detector_studies/delphes/analysis_resolution.py
@@ -0,0 +1,67 @@
+
+
+import sys, os, glob, math
+import ROOT
+import logging
+
+logging.basicConfig(format='%(levelname)s: %(message)s')
+logger = logging.getLogger("fcclogger")
+logger.setLevel(logging.INFO)
+
+
+ROOT.gROOT.SetBatch()
+ROOT.gStyle.SetOptStat(0)
+ROOT.gStyle.SetOptTitle(0)
+
+ROOT.EnableImplicitMT() # use all cores
+#ROOT.DisableImplicitMT() # single core
+
+# load libraries
+ROOT.gSystem.Load("libFCCAnalyses")
+fcc_loaded = ROOT.dummyLoader()
+ROOT.gInterpreter.Declare("using namespace FCCAnalyses;")
+ROOT.gInterpreter.Declare('#include "functions.h"')
+
+
+
+bins_p = (250, 0, 250)
+bins_res = (10000, -0.05, 0.05)
+
+def analysis(input_files, output_file):
+
+    df = ROOT.RDataFrame("events", input_files)
+
+    df = df.Alias("Particle0", "Particle#0.index")
+    df = df.Alias("Particle1", "Particle#1.index")
+    df = df.Alias("MCRecoAssociations0", "MCRecoAssociations#0.index")
+    df = df.Alias("MCRecoAssociations1", "MCRecoAssociations#1.index")
+
+    df = df.Alias("Muons", "Muon#0.index")
+    df = df.Define("muons_all", "FCCAnalyses::ReconstructedParticle::get(Muons, ReconstructedParticles)")
+    df = df.Define("muons_p", "FCCAnalyses::ReconstructedParticle::get_p(muons_all)")
+
+    muons_p = df.Histo1D(("muons_p", "", *bins_p), "muons_p")
+
+
+    # get resolution = reco/gen momentum
+    df = df.Define("muons_res_p", "FCCAnalyses::leptonResolution(muons_all, MCRecoAssociations0, MCRecoAssociations1, ReconstructedParticles, Particle, 0)")
+    muons_res_p = df.Histo1D(("muons_res_p", "", *bins_res), "muons_res_p")
+
+
+
+    fout = ROOT.TFile(output_file, "RECREATE")
+    muons_p.Write()
+    muons_res_p.Write()
+    fout.Close()
+
+
+
+if __name__ == "__main__":
+
+    input_files, output_file = ["samples/IDEA_2T_Zmumu_ecm240.root"], "output/IDEA_2T_Zmumu_ecm240.root"
+    input_files, output_file = ["samples/CLD_2T_Zmumu_ecm240.root"], "output/CLD_2T_Zmumu_ecm240.root"
+
+    logger.info(f"Start analysis")
+    analysis(input_files, output_file)
+    logger.info(f"Done! Output saved to {output_file}")
+
